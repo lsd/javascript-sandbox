41f7846b6c8998e2f6a325e22e320dcb328fabc3
Husam Machlovi <husam@withpulp.com>
2015-11-27 20:19:25 -0500

    game board definition

--- chess/js/board.js
+++ chess/js/board.js
@@ -1,3 +1,8 @@
 var GameBoard = {};
 
 GameBoard.pieces = new Array(BRD_SQ_NUM);
+GameBoard.side = COLOURS.WHITE;
+GameBoard.fiftyMove = 0;
+GameBoard.hisPly = 0;
+GameBoard.ply = 0;
+GameBoard.castlePerm = 0;
