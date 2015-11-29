680f974f3414408f6bd07b0701f7e8c2dd4578e6
Husam Machlovi <husam@withpulp.com>
2015-11-29 13:50:56 -0500

    position hask hey

--- chess/js/board.js
+++ chess/js/board.js
@@ -9,37 +9,9 @@ GameBoard.side = COLOURS.WHITE;
 GameBoard.fiftyMove = 0;
 GameBoard.hisPly = 0;
 GameBoard.ply = 0;
+GameBoard.enPas = 0;
 GameBoard.castlePerm = 0;
 GameBoard.material = new Array(2); // WHITE,BLACK material of pieces
 GameBoard.pceNum = new Array(13); // indexed by Pce
 GameBoard.pList = new Array(14 * 10);
-
-
-/*
-
-loop (pieces[])
-if(piece on sq == Side tomove)
-then genmoves() for piece on sq
-
-
-sqOfpiece = PlistArray[index];
-
-index?
-
-wP * 10 + wPNum -> 0 based index of num of pieces(GameBoard.pceNum)
-wN * 10 + wNnum
-
-say we have 4 white pawns GameBoard.pceNum[wP] = 4
-
-for(pceNum = 0; pceNum < GameBoard.pceNum[wP]; ++pceNum) {
-	sq = PlistArray[wP * 10 + pceNum]
-
-}
-
-sq1 = PlistArray[wP * 10 + 0]
-sq2 = PlistArray[wP * 10 + 1]
-sq3 = PlistArray[wP * 10 + 2]
-sq4 = PlistArray[wP * 10 + 3]
-
-wP 10 -> 19
-*/
+GameBoard.posKey = 0;
