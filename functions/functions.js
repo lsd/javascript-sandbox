5a56fe461049f3123445f8160377172a840572de
Husam Machlovi <husam@withpulp.com>
2015-11-30 18:06:07 -0500

    function prac

---
+++ functions/functions.js
@@ -0,0 +1,16 @@
+//write a function that takes an argument and returns that arugment
+
+var function printer(x) {
+  return x;
+}
+
+fart = printer('fart');
+console.log(fart);
+
+// write two binary functions add and mull, that take two numbers and return their sum and product
+
+// var init = function init() {
+//   printer(fart);
+// };
+//
+// console.log(init);
