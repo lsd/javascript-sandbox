a5414dde93e138dadc27412487ea69d01283ed0c
Husam Machlovi <husam@withpulp.com>
2015-12-01 00:19:41 -0500

    closures

--- functions/functions.js
+++ functions/functions.js
@@ -32,9 +32,26 @@ function identityf(x) {
 //write a function that adds from two invocations
 function addf(x) {
   return function (y) {
-    x + y;
+    return x + y;
   };
 }
+
+/*
+write a function that takes a binary function, and makes it callable with two invocations
+  addf = applyf(add);
+  addf(3)(4)
+  applyf(mul)(5)(6)
+*/
+
+function applyf(binary) {
+  return function (x) {
+    return function (y) {
+      return binary(x, y);
+    };
+  };
+}
+
+
 
 // var init = function init() {
 //   printer(fart);
