b6fe31b42eaff9193b69e844aefe970a40e65f1d
Husam Machlovi <husam@withpulp.com>
2015-12-28 13:19:12 -0500

    array ex

--- functions/arrays.js
+++ functions/arrays.js
@@ -4,11 +4,22 @@
 //For each choice, log to the screen a string like: "My #1 choice is blue."
 //Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
 
-powerStones = ['emerald', 'sapphire', 'ruby', 'hearth']
+var choices = ['red', 'orange', 'pink', 'yellow'];
+for (var i = 0; i < choices.length; i++) {
+    console.log('My #' + (i + 1) + ' choice is ' + choices[i]);
+}
 
-for (var i=0; i<powerStones.length; i++) {
-     var choice = powerStones[i];
-     console.log("My #" + i + ' choice is ' + choice);
-};
-//     
-//     for (var i = 0; i < times; i++)
+for (var i = 0; i < choices.length; i++) {
+    var choiceNum = i + 1;
+    var choiceNumSuffix;
+    if (choiceNum == 1) {
+        choiceNumSuffix = 'st';
+    } else if (choiceNum == 2) {
+        choiceNumSuffix = 'nd';
+    } else if (choiceNum == 3) {
+        choiceNumSuffix = 'rd';
+    } else {
+        choiceNumSuffix = 'th';
+    }
+    console.log('My ' + choiceNum + choiceNumSuffix + ' choice is ' + choices[i]);
+}
